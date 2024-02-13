01c1df58130699098191739cd950f0abc1e7d691
gideonzozingao <gideonzozingao@LT2308VASGIDENZ.vodafonepng.local>
2024-02-13 15:24:13 +1000

    Added file Uploads to the report routes(zuqongtech)

--- app/routes/reports.py
+++ app/routes/reports.py
@@ -1,10 +1,30 @@
-from fastapi import FastAPI, File, HTTPException, Body, Cookie, File, Form, Header, Path, Query, status, UploadFile, APIRouter,Depends
+from typing import Annotated, List
+from fastapi import (
+    FastAPI,
+    File,
+    HTTPException,
+    Body,
+    Cookie,
+    File,
+    Form,
+    Header,
+    Path,
+    Query,
+    status,
+    File,
+    UploadFile,
+    APIRouter,
+    Depends,
+    Request,
+)
+
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from ..dependencies.dependencies import get_token_header
-from datetime import date,datetime
+from datetime import date, datetime
+
 router = APIRouter(
     prefix="/reports",
     tags=["reports"],
@@ -13,26 +33,30 @@ router = APIRouter(
 )
 
 Base = declarative_base()
+
+
 class RefuelingReport(BaseModel):
     date: date
-    site_id:str
-    site_name:str
-    fuel_level_before:str
-    fuel_level_after:str
+    site_id: str
+    site_name: str
+    fuel_level_before: str
+    fuel_level_after: str
     comment: str
-    running_hours:str
+    running_hours: str
+
 
 class SupplyPickupReport(BaseModel):
     date: date
-    pickup_location:str
-    contractor_name:str
-    vehicle_rego:str
-    number_of_drum:int
+    pickup_location: str
+    contractor_name: str
+    vehicle_rego: str
+    number_of_drum: int
     sites: str
-    oil_qty:float
-    coolant_qty:float
-    comment:str
-    
+    oil_qty: float
+    coolant_qty: float
+    comment: str
+
+
 SQLALCHEMY_DATABASE_URL = (
     "mysql+mysqlconnector://gideonzozingao:password123@localhost/mno"
 )
@@ -41,14 +65,21 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 @router.get("/", tags=["reports"])
 async def read_reports():
     return [{"report": "This is a report 1"}, {"report": "This is a report 2"}]
 
+
 @router.post("/genset_refueling")
-async def read_refuelingreport(report:RefuelingReport):
-    return {"report": report}
+async def read_refuelingreport(
+    report: RefuelingReport, files: List[UploadFile] = File(...)
+):
+    return {"report": report, "file_size": len(files)}
+
 
 @router.post("/supply_pickup")
-async def read_refuelingreport(report1:SupplyPickupReport):
-    return {"report": report1}
+async def read_refuelingreport(
+    report1: SupplyPickupReport, files: List[UploadFile] = File(...)
+):
+    return {"report": report1, "file_size": len(files)}
